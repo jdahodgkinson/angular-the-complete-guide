2c13311de2c2fb82d2cc63ca2a46d21476f31470
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-01-11 20:03:19 +0000

    completed databinding assignment

--- section2-the-basics/src/app/app.module.ts
+++ section2-the-basics/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { FormsModule } from '@angular/forms';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent,
+    UsernameComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ section2-the-basics/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  template: '<h2>Success!</h2>',
+  styles: ['h2 { color: forestgreen; }']
+})
+
+export class SuccessAlertComponent {}

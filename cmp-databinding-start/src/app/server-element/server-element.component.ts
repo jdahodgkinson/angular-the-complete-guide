722acae47965561320e821d4bbbbc1d5b4bbaef4
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-01-24 15:32:57 +0000

    finished section 5

---
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -0,0 +1,31 @@
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges
+} from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+  // encapsulation: ViewEncapsulation.Emulated
+})
+export class ServerElementComponent implements OnInit, OnChanges {
+  @Input() element: { type: string, name: string, content: string };
+
+  constructor() {
+    console.log('constructor called...');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called...');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called...');
+  }
+
+}
